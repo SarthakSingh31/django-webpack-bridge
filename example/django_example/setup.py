0c764c351fa23ca467059f8002aa690497d6eb06
Sarthak Singh <ss269@uw.edu>
2021-02-26 09:45:02 -0800

    WIP: Added some starter layout

---
+++ example/django_example/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name = 'django-webpack-bridge-example',
+    long_description_content_type="text/markdown",
+    author = 'Sarthak Singh',
+    author_email = 'ss269@uw.edu',
+    # install_requires=[
+    #     ''
+    # ],
+    classifiers = [
+        'Programming Language :: Python :: 3 :: Only',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
