9436d3ba4a58e5883145873fd8d237fb9af6e338
Sarthak Singh <ss269@uw.edu>
2021-02-26 17:54:20 -0800

    Reloading is working

--- example/webpack_example/webpack.config.js
+++ example/webpack_example/webpack.config.js
@@ -1,6 +1,9 @@
 'use strict'
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DjangoBridgePlugin = require('django-webpack-bridge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   optimization: {
@@ -17,7 +20,33 @@ module.exports = {
     chunkFilename: '[id]-[chunkhash].js',
     publicPath: '/',
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
+      },
+    ]
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new DjangoBridgePlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[fullhash].css',
+      chunkFilename: '[id]-[chunkhash].css',
+    }),
+    new VueLoaderPlugin(),
   ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    },
+  }
 }
